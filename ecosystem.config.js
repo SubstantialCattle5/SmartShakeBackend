2e9d480676953ae9d809fcd999d0c105a66316e4
substantialcattle5 <nilaynathsharan16@gmail.com>
2025-08-26 15:01:29 +0530

    fixed link issue in pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'smartshake-backend',
-    script: 'dist/index.js',
+    script: 'dist/src/index.js',
     instances: 1,
     autorestart: true,
     watch: false,
